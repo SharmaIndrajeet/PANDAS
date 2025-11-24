58073153f8e0a6cf34e49da4c799fae2e0946849
SharmaIndrajet <indrajeetjadoo15@gmail.com>
2025-11-24 16:12:34 +0530

    day1

---
+++ Day1/main.py
@@ -0,0 +1,13 @@
+import pandas as  pd 
+import numpy as np   
+
+# data = np.array(['a' , 'b' , 'c' , 'd'])
+# s= pd.Series(data)
+# print (s)
+
+data1 = {'a':0,
+         'b':1,
+         'c':2}
+
+sa = pd.Series(data1)
+print (sa)
